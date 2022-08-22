eb74fb8828f278928ecfce2c5473d244913c23d7
Acc3l3rat0r <kraje.robert@gmail.com>
2022-08-22 23:17:05 +0300

    1.1 transactional fix

--- src/main/java/com/kata/study/pp_3_1_1/service/UserServiceImpl.java
+++ src/main/java/com/kata/study/pp_3_1_1/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.kata.study.pp_3_1_1.service;
 import com.kata.study.pp_3_1_1.model.User;
 import com.kata.study.pp_3_1_1.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,16 +17,19 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         this.userRepository.save(user);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUserById(long id) {
         Optional<User> optional = userRepository.findById(id);
         User user = null;
@@ -38,6 +42,7 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
+    @Transactional
     public void deleteUserById(long id) {
         this.userRepository.deleteById(id);
     }
